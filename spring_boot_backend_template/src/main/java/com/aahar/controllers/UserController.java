a7090ba24e64be781f35a6fd97a8f86860ebe80e
Tarun Kumar <tarunkumar@Taruns-MacBook-Air.local>
2025-07-23 15:49:24 +0530

    hi

--- spring_boot_backend_template/src/main/java/com/aahar/controllers/UserController.java
+++ spring_boot_backend_template/src/main/java/com/aahar/controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.aahar.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+@RestController
+@AllArgsConstructor
 public class UserController {
 
 }
--- spring_boot_backend_template/src/main/java/com/aahar/entities/Restaurant.java
+++ spring_boot_backend_template/src/main/java/com/aahar/entities/Restaurant.java
@@ -12,10 +12,7 @@ public class Restaurant extends BaseEntity{
 	private double totalRating;
 	private boolean isOnline;
 	
-<<<<<<< HEAD
-=======
 	
->>>>>>> 8aa1d9e9e2f4a7ad4e52568220a0acab8d85fd19
 	public Restaurant(String restaurantName, String restauratDescription, boolean isVeg, double avgCost, double ratingSum,
 			double totalRating, boolean isOnline) {
 		super();
