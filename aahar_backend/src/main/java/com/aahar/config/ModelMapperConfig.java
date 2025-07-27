d0a30c3c01ab1c46da57078c5ebb013ff5cad67f
Maithilik1605 <maithili.kontamwar@gmail.com>
2025-07-28 04:06:01 +0530

    Controller1

--- aahar_backend/src/main/java/com/aahar/config/ModelMapperConfig.java
+++ aahar_backend/src/main/java/com/aahar/config/ModelMapperConfig.java
@@ -7,20 +7,20 @@ import org.springframework.context.annotation.Configuration;
 import com.aahar.entities.RestaurantAddress;
 import com.aahar.dto.RestaurantAddressDTO;
 
-@Configuration
-public class ModelMapperConfig {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper mapper = new ModelMapper();
-
-        // Map Restaurant -> restaurantId inside DTO
-        mapper.typeMap(RestaurantAddress.class, RestaurantAddressDTO.class)
-              .addMappings(m -> m.map(
-                      src -> (src.getRestaurant() != null ? src.getRestaurant().getId() : null),
-                      RestaurantAddressDTO::setRestaurantId
-              ));
-
-        return mapper;
-    }
-}
+//@Configuration
+//public class ModelMapperConfig {
+//
+//    @Bean
+//    public ModelMapper modelMapper() {
+//        ModelMapper mapper = new ModelMapper();
+//
+//        // Map Restaurant -> restaurantId inside DTO
+//        mapper.typeMap(RestaurantAddress.class, RestaurantAddressDTO.class)
+//              .addMappings(m -> m.map(
+//                      src -> (src.getRestaurant() != null ? src.getRestaurant().getId() : null),
+//                      RestaurantAddressDTO::setRestaurantId
+//              ));
+//
+//        return mapper;
+//    }
+//}
