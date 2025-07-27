ae19ce7bd32bf27af2ab02c252ae802e764cdec9
Tarun Kumar <tarunkumar@Taruns-MacBook-Air.local>
2025-07-27 13:14:24 +0530

    Written all the customer apis(controllers,service,dao,dto,exceptions)

--- aahar_backend/src/main/java/com/aahar/dto/CustomerDTO.java
+++ aahar_backend/src/main/java/com/aahar/dto/CustomerDTO.java
@@ -1,5 +1,8 @@
 package com.aahar.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +14,12 @@ public class CustomerDTO {
 	private String lastName;
 	
 	private String email;
-	
+	/*this DTO is used in both the cases 
+		1. while adding the customer
+		2.while displaying the customer profile
+	this jsonproperty annotation will hide the password while display
+	*
+	*/
+	@JsonProperty(access = Access.WRITE_ONLY)
 	private String password;
 }
