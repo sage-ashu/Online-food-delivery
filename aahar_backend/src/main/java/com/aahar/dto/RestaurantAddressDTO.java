401d18f15fb7176146fd2cb03a7b8565dbc801ff
Maithilik1605 <maithili.kontamwar@gmail.com>
2025-07-28 03:30:00 +0530

    Controller

---
+++ aahar_backend/src/main/java/com/aahar/dto/RestaurantAddressDTO.java
@@ -0,0 +1,22 @@
+package com.aahar.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RestaurantAddressDTO {
+    private String phoneNo;
+    private String address1;
+    private String address2;
+    private String address3;
+    private String city;
+    private String pinCode;
+    private double latitude;
+    private double longitude;
+
+    private Long restaurantId;
+}
+
