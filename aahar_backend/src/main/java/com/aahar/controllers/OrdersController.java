16607ed76f8dae0e079264274802e37b2618eeac
tanmay-Git21 <tanmay.github.21@gmail.com>
2025-07-29 16:05:46 +0530

    Update order status

--- aahar_backend/src/main/java/com/aahar/controllers/OrdersController.java
+++ aahar_backend/src/main/java/com/aahar/controllers/OrdersController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.aahar.dto.CustomerOrderResponseDTO;
 import com.aahar.dto.AddOrderDTO;
 import com.aahar.dto.RestaurantOrderResponseDTO;
+import com.aahar.dto.UpdateOrderStatusDTO;
 import com.aahar.services.OrdersService;
-
+import com.aahar.servicesImplementaion.DistanceServiceImplementation;
 import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/orders")
 @AllArgsConstructor
 public class OrdersController {
+
+    private final DistanceServiceImplementation distanceServiceImplementation;
 	public final OrdersService ordersService;
+
+
 
 	//1. add Orders by customer id and restaurant id
 //	@PostMapping("")
@@ -45,6 +52,68 @@ public class OrdersController {
 	}
 	
 	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	//4. update order status by order id
+	
+    @PutMapping("/{orderId}/status")
+    public ResponseEntity<?> updateOrderStatus(@PathVariable Long orderId,
+                                               @RequestBody UpdateOrderStatusDTO dto) {
+//    	System.out.println(dto.getStatus());
+        ordersService.updateOrderStatus(orderId, dto.getStatus());
+        return ResponseEntity.ok("Order status updated successfully.");
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- aahar_backend/src/main/java/com/aahar/services/OrdersService.java
+++ aahar_backend/src/main/java/com/aahar/services/OrdersService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.aahar.dto.CustomerOrderResponseDTO;
 import com.aahar.dto.AddOrderDTO;
 import com.aahar.dto.RestaurantOrderResponseDTO;
+import com.aahar.entities.OrderStatus;
 
 public interface OrdersService {
 
@@ -13,5 +14,14 @@ public interface OrdersService {
 	List<RestaurantOrderResponseDTO> getRestaurantOrders(Long restaurantId);
 
 	void addOrder(AddOrderDTO orderDTO);
+	
+	
+	
+	
+	
+	
+	
+	
+	void updateOrderStatus(Long orderId, OrderStatus newStatus);
 
 }
